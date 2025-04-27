db303308b0fd5499930956cdb6f96141df66c10a
SeshuTarapatla <seshu.tarapatla@gmail.com>
2025-04-27 18:50:08 +0530

    log newlines

--- src/my_modules/logger.py
+++ src/my_modules/logger.py
@@ -15,14 +15,26 @@ class log:
     @staticmethod
     def info(msg: Any) -> None:
         """Log info message."""
-        console.print(f" [[blue]INFO[/]] : {msg}")
+        console.print(f" [[blue]INFO[/]] : {log._handle_new_lines(msg)}")
 
     @staticmethod
     def warn(msg: Any) -> None:
         """Log warning message."""
-        console.print(f" [[yellow]WARN[/]] : {msg}")
+        console.print(f" [[yellow]WARN[/]] : {log._handle_new_lines(msg)}")
 
     @staticmethod
     def error(msg: Any) -> None:
         """Log error message."""
-        console.print(f"[[red]ERROR[/]] : {msg}")
+        console.print(f"[[red]ERROR[/]] : {log._handle_new_lines(msg)}")
+    
+    @staticmethod
+    def _handle_new_lines(msg: Any) -> str:
+        """Handle any starting newlines before logging. Ex: '\\nHello world'"""
+        msg = str(msg)
+        i = 0
+        for i, newline in enumerate(msg):
+            if newline == "\n":
+                print(newline)
+            else:
+                break
+        return msg[i:]
