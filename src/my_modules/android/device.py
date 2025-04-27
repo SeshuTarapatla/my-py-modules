5f0f3aa70a2708662471dd6e8eaf1126140f77d1
SeshuTarapatla <seshu.tarapatla@gmail.com>
2025-04-28 02:21:38 +0530

    hierarchy methods

--- src/my_modules/android/device.py
+++ src/my_modules/android/device.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from uiautomator2 import Device as _Device
 
 __all__ = ["Device"]
@@ -6,3 +7,20 @@ __all__ = ["Device"]
 class Device(_Device):
     def __init__(self, serial: str):
         super().__init__(serial)
+
+    def dump_hierarchy(
+        self,
+        compressed=False,
+        pretty=False,
+        max_depth: int | None = None,
+        dest: Path = Path("./hierarchy.txt"),
+    ) -> str:
+        """Modified dump_hierarchy to dump to file instead of console."""
+        data = super().dump_hierarchy(compressed, pretty, max_depth)
+        dest.write_text(data)
+        return "dumped"
+
+    def search_hierarchy(self, text: str) -> list[str]:
+        """Search wrapper that returns lines in ui hierarchy dump that contains given text content."""
+        data = super().dump_hierarchy().splitlines()
+        return [line.strip().split(" checkable")[0] + ">" for line in data if text in line]
